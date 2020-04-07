85cc020caa62f3ffa68157da804ea71ecafed6bc
xiangli13 <34529989+xiangli13@users.noreply.github.com>
2020-04-07 15:18:42 +0800

    use binary_crossentropy
    use binary_crossentropy to overcome the numerical stability

--- circle_loss.py
+++ circle_loss.py
@@ -31,9 +31,16 @@ class CircleLoss(Model):
         alpha_n = K.relu(sim_n + self.margin)
         margin_p = 1 - self.margin
         margin_n = -self.margin
-        loss_p = K.mean(K.sum(K.exp(-self.scale * alpha_p * (sim_p - margin_p)), axis=1))
-        loss_n = K.mean(K.sum(K.exp(self.scale * alpha_n * (sim_n - margin_n)), axis=1))
-        return K.log(1 + loss_p * loss_n)
+	
+	logit_p = tf.reshape(self.scale * alpha_p * (sim_p - margin_p), (-1, 1))
+        logit_n = tf.reshape(self.scale * alpha_n * (sim_n - margin_n), (-1, 1))
+
+        label_p = tf.ones_like(logit_p)
+        label_n = tf.zeros_like(logit_n)
+
+        return K.mean(binary_crossentropy(
+                        tf.concat([label_p, label_n], axis=0), tf.concat([logit_p, logit_n], axis=0), 
+                        from_logits=True))
     
     def compute_output_shape(self, input_shape):
         return (1,)
